ef9cb7622f62a475bf35a221d437c44aa9f3a692
eduard plyushchevich <eduardatapovic@gmail.com>
2026-04-29 00:20:10 +0300

    Add js constants for auth

---
+++ src/web/static/auth/js/messages.js
@@ -0,0 +1,10 @@
+export const MESSAGES = {
+  usernameRequired: "Username is required",
+  usernameTooShort: (min) => `Username must be at least ${min} characters`,
+  emailRequired: "Email is required",
+  emailInvalid: "Invalid email",
+  passwordRequired: "Password is required",
+  passwordTooShort: (min) => `Password must be at least ${min} characters`,
+  confirmRequired: "Please confirm your password",
+  passwordsMismatch: "Passwords do not match",
+};
