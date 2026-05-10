459895bfb1ef2b623692a45e852660715581f396
eduard plyushchevich <eduardatapovic@gmail.com>
2026-05-10 15:51:28 +0300

    Change naming convention

--- src/infrastructure/db/base.py
+++ src/infrastructure/db/base.py
@@ -1,11 +1,10 @@
 import uuid
 
 from sqlalchemy import MetaData, text
-from sqlalchemy.dialects.postgresql.base import UUID
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 NAMING_CONVENTION = {
-    "ix": "%(column_0_label)s_idx",
+    "ix": "%(table_name)s_%(column_0_name)s_idx",
     "uq": "%(table_name)s_%(column_0_name)s_key",
     "ck": "%(table_name)s_%(constraint_name)s_check",
     "fk": "%(table_name)s_%(column_0_name)s_fkey",
@@ -20,10 +19,8 @@ class Base(DeclarativeBase):
         naming_convention=NAMING_CONVENTION,
     )
 
-    id: Mapped[UUID] = mapped_column(
-        UUID(as_uuid=True),
+    id: Mapped[uuid.UUID] = mapped_column(
         primary_key=True,
-        default=uuid.uuid4,
         server_default=text("gen_random_uuid()"),
     )
 
