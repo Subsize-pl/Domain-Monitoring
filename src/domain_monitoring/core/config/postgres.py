7cf010d288d3d5c894c44f1119641d4833c2e390
eduard plyushchevich <eduardatapovic@gmail.com>
2026-05-17 18:47:54 +0300

    refactor: remove debug info

--- src/domain_monitoring/core/config/postgres.py
+++ src/domain_monitoring/core/config/postgres.py
@@ -13,13 +13,6 @@ class PostgresSettings(BaseModel):
 
     @property
     def asyncpg_url(self) -> str:
-        print(
-            (
-                f"{self.scheme}://{self.user}:"
-                f"{quote_plus(self.password.get_secret_value())}@"
-                f"{self.host}:{self.port}/{self.db_name}"
-            )
-        )
         return (
             f"{self.scheme}://{self.user}:"
             f"{quote_plus(self.password.get_secret_value())}@"
